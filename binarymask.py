e43faa885d315c33c848d0db2470f3357e1936ab
Wei (Wayne) Hu <wei.hu@duke.edu>
2021-10-27 12:35:59 -0400

    Save `.tif` mask files with `binarymask.py`

--- binarymask.py
+++ binarymask.py
@@ -1,27 +1,40 @@
+import json
+from pathlib import Path
+
 import pandas as pd
 import numpy as np
+from skimage import io
 from skimage.draw import polygon, line
-import json
 from PyQt5.QtWidgets import QFileDialog, QApplication
 
 def produce_mask(path):
+
 	df=pd.read_csv(path)
 	img=np.zeros((df['width'][0], df['height'][0]))
 	U=df['Object'].unique()
+
 	for i in U:
 		dfsubstr=df[df['Object']==i]
+		
 		if dfsubstr['Type'].values[0]=='Polygon':
-			cc,rr=polygon(dfsubstr['X'].values, dfsubstr['Y'].values)
+			cc,rr=polygon(dfsubstr['X'].values - 1, dfsubstr['Y'].values)
 			img[rr,cc]=1
+		
 		elif dfsubstr['Type'].values[0]=='Line':
 			for j in range(dfsubstr.shape[0]-1):
 				r0, c0 = int(dfsubstr['X'].values[j]), int(dfsubstr['Y'].values[j])
 				r1, c1 = int(dfsubstr['X'].values[j+1]), int(dfsubstr['Y'].values[j+1])
 				cc,rr=line(r0, c0, r1, c1)
 				img[rr,cc]=1
+		
 		else: #A point
 			img[int(dfsubstr['X']),int(dfsubstr['Y'])]=1
-	np.savez_compressed(path[:-4], img)
+	
+	fname = str(path.name).split('.')[0].replace('_RGB', '_GT') + '.tif'
+	io.imsave(
+		path.parent / fname,
+		img.astype(np.uint8) * 255
+	)
 	return
 
 if __name__ == '__main__':
@@ -32,6 +45,7 @@ if __name__ == '__main__':
 	dialogue.setDefaultSuffix('csv')
 	dialogue.setFileMode(QFileDialog.ExistingFiles)
 	dialogue.exec()
-	path=dialogue.selectedFiles()
+	filepaths=dialogue.selectedFiles()
 
-	[produce_mask(p) for p in path]
+	for p in filepaths:
+		produce_mask(Path(p)) 
